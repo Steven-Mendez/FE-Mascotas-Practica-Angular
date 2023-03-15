40e0cd5b03cf3674a001d1979005a9646015cd15
Steven Mendez <stevenampaiz@gmail.com>
2023-03-15 10:09:05 -0600

    Routeo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { ListaMascotaComponent } from './components/lista-mascota/lista-mascota.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'listMascotas', pathMatch: 'full' },
+  { path: 'listMascotas', component: ListaMascotaComponent },
+  { path: 'agregarMascota', component: AgregarEditarMascotaComponent },
+  { path: 'verMascota/:id', component: VerMascotaComponent },
+  { path: 'editarMascota/:id', component: AgregarEditarMascotaComponent },
+  { path: '**', redirectTo: 'listMascotas', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
